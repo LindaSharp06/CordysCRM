10fffb5d7d3d2dc7a19e770990e78172f8a0ec37
LindaSharp06 <linda0729sharp@outlook.com>
2024-12-31 14:22:41 +0800

    build: pxToRem

--- frontend/packages/mobile/rsbuild.config.ts
+++ frontend/packages/mobile/rsbuild.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginVue } from '@rsbuild/plugin-vue';
 import { VantResolver } from '@vant/auto-import-resolver';
+import postcssPxtorem from 'postcss-pxtorem';
 import AutoImport from 'unplugin-auto-import/rspack';
 import Components from 'unplugin-vue-components/rspack';
 
@@ -27,6 +28,20 @@ export default defineConfig({
           resolvers: [VantResolver()],
         }),
       ],
+    },
+    postcss: {
+      postcssOptions: {
+        plugins: [
+          postcssPxtorem({
+            rootValue: 16, // 基准值
+            propList: ['*'], // 需要转换的属性列表
+            selectorBlackList: [], // 忽略的选择器
+            replace: true, // 替换而不是添加回退
+            mediaQuery: false, // 允许在媒体查询中转换px
+            minPixelValue: 0, // 最小的转换数值
+          }),
+        ],
+      },
     },
   },
 });
