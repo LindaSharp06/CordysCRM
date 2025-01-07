d11fdd55d1e8c125ea14c5c4d6ef5505e233f59f
LindaSharp06 <linda0729sharp@outlook.com>
2025-01-07 18:42:01 +0800

    refactor: Improve Internationalization

--- backend/crm/src/main/java/io/cordys/crm/system/domain/Schedule.java
+++ backend/crm/src/main/java/io/cordys/crm/system/domain/Schedule.java
@@ -1,49 +1,38 @@
 package io.cordys.crm.system.domain;
 
-import io.cordys.common.groups.*;
+import io.cordys.common.domain.BaseModel;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.*;
-import java.io.Serial;
-import java.io.Serializable;
 import lombok.Data;
 
+import java.io.Serial;
+
 @Data
-public class Schedule implements Serializable {
-    @Schema(description = "", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "{schedule.id.not_blank}", groups = {Updated.class})
-    @Size(min = 1, max = 50, message = "{schedule.id.length_range}", groups = {Created.class, Updated.class})
+public class Schedule extends BaseModel {
+    @Schema(description = "id")
     private String id;
 
-    @Schema(description = "qrtz UUID")
+    @Schema(description = "Qrtz UUID")
     private String key;
 
     @Schema(description = "组织ID")
     private String organizationId;
 
     @Schema(description = "执行类型 cron", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "{schedule.type.not_blank}", groups = {Created.class})
-    @Size(min = 1, max = 50, message = "{schedule.type.length_range}", groups = {Created.class, Updated.class})
     private String type;
 
     @Schema(description = "cron 表达式", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "{schedule.value.not_blank}", groups = {Created.class})
-    @Size(min = 1, max = 255, message = "{schedule.value.length_range}", groups = {Created.class, Updated.class})
     private String value;
 
     @Schema(description = "Schedule Job Class Name", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "{schedule.job.not_blank}", groups = {Created.class})
-    @Size(min = 1, max = 64, message = "{schedule.job.length_range}", groups = {Created.class, Updated.class})
     private String job;
 
-    @Schema(description = "资源类型 API_IMPORT,API_SCENARIO,UI_SCENARIO,LOAD_TEST,TEST_PLAN,CLEAN_REPORT,BUG_SYNC", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "{schedule.resource_type.not_blank}", groups = {Created.class})
-    @Size(min = 1, max = 50, message = "{schedule.resource_type.length_range}", groups = {Created.class, Updated.class})
+    @Schema(description = "资源类型", requiredMode = Schema.RequiredMode.REQUIRED)
     private String resourceType;
 
     @Schema(description = "是否开启")
     private Boolean enable;
 
-    @Schema(description = "资源ID，api_scenario ui_scenario load_test")
+    @Schema(description = "资源ID")
     private String resourceId;
 
     @Schema(description = "创建人")
