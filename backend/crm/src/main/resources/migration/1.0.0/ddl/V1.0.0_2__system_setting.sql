19918e5aaad958a3f1143c7af6226fda0b1118e9
song-cc-rock <changchang.song@fit2cloud.com>
2025-01-08 13:41:23 +0800

    feat: add lead pool ddl

--- backend/crm/src/main/resources/migration/1.0.0/ddl/V1.0.0_2__system_setting.sql
+++ backend/crm/src/main/resources/migration/1.0.0/ddl/V1.0.0_2__system_setting.sql
@@ -244,11 +244,11 @@ CREATE TABLE sys_module
     `organization_id` VARCHAR(32)  NOT NULL COMMENT '组织id',
     `name`            VARCHAR(255) NOT NULL COMMENT '模块名称(国际化KEY)',
     `enable`          BIT(1)       NOT NULL DEFAULT 1 COMMENT '启用/禁用',
-    `pos`             BIGINT(255)  NOT NULL COMMENT '自定义排序',
+    `pos`             BIGINT  NOT NULL COMMENT '自定义排序',
     `create_user`     VARCHAR(32)  NOT NULL COMMENT '创建人',
-    `create_time`     BIGINT(255)  NOT NULL COMMENT '创建时间',
+    `create_time`     BIGINT  NOT NULL COMMENT '创建时间',
     `update_user`     VARCHAR(32)  NOT NULL COMMENT '修改人',
-    `update_time`     BIGINT(255)  NOT NULL COMMENT '修改时间',
+    `update_time`     BIGINT  NOT NULL COMMENT '修改时间',
     PRIMARY KEY (id)
 ) COMMENT = '模块设置'
     ENGINE = InnoDB
@@ -414,6 +414,75 @@ COLLATE = utf8mb4_general_ci;
 
 CREATE INDEX idx_resource ON sys_department(resource ASC);
 CREATE INDEX idx_organization_id ON sys_department(organization_id ASC);
+
+CREATE TABLE lead_pool(
+    `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
+    `name` VARCHAR(255) NOT NULL   COMMENT '线索池名称' ,
+    `scope_id` VARCHAR(32) NOT NULL   COMMENT '成员id' ,
+    `organization_id` VARCHAR(32) NOT NULL   COMMENT '组织架构id' ,
+    `owner_id` VARCHAR(32) NOT NULL   COMMENT '管理员id' ,
+    `enable` BIT(1) NOT NULL  DEFAULT 1 COMMENT '启用/禁用' ,
+    `recycled` BIT(1) NOT NULL  DEFAULT 1 COMMENT '是否自动回收' ,
+    `create_time` BIGINT NOT NULL   COMMENT '创建时间' ,
+    `update_time` BIGINT NOT NULL   COMMENT '更新时间' ,
+    `create_user` VARCHAR(32) NOT NULL   COMMENT '创建人' ,
+    `update_user` VARCHAR(32) NOT NULL   COMMENT '更新人' ,
+    PRIMARY KEY (id)
+)  COMMENT = '线索池'
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_general_ci;
+
+CREATE INDEX idx_organization_id ON lead_pool(organization_id ASC);
+CREATE INDEX idx_owner_id ON lead_pool(owner_id ASC);
+CREATE INDEX idx_scope_id ON lead_pool(scope_id ASC);
+
+CREATE TABLE lead_pool_pick_rule(
+    `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
+    `limit_on_number` BIT(1) NOT NULL  DEFAULT 1 COMMENT '是否限制领取数量' ,
+    `pick_number` INT    COMMENT '领取数量' ,
+    `limit_pre_owner` BIT(1) NOT NULL  DEFAULT 1 COMMENT '是否限制前归属人领取' ,
+    `pick_interval_days` INT    COMMENT '领取间隔天数' ,
+    `create_user` VARCHAR(32) NOT NULL   COMMENT '创建人' ,
+    `create_time` BIGINT NOT NULL   COMMENT '创建时间' ,
+    `update_user` VARCHAR(32) NOT NULL   COMMENT '更新人' ,
+    `update_time` BIGINT NOT NULL   COMMENT '更新时间' ,
+    PRIMARY KEY (id)
+)  COMMENT = '线索池领取规则'
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE lead_pool_recycle_rule(
+    `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
+    `expire_notice` BIT(1) NOT NULL  DEFAULT 1 COMMENT '到期提醒' ,
+    `notice_days` INT(255)    COMMENT '提前提醒天数' ,
+    `create_time` BIGINT NOT NULL   COMMENT '创建时间' ,
+    `update_time` BIGINT NOT NULL   COMMENT '更新时间' ,
+    `create_user` VARCHAR(32) NOT NULL   COMMENT '创建人' ,
+    `update_user` VARCHAR(32) NOT NULL   COMMENT '更新人' ,
+    PRIMARY KEY (id)
+)  COMMENT = '线索池回收规则'
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE lead_pool_relation(
+    `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
+    `pool_id` VARCHAR(32) NOT NULL   COMMENT '线索池id' ,
+    `lead_id` VARCHAR(32) NOT NULL   COMMENT '线索id' ,
+    `create_time` BIGINT NOT NULL   COMMENT '创建时间' ,
+    `update_time` BIGINT NOT NULL   COMMENT '更新时间' ,
+    `create_user` VARCHAR(32) NOT NULL   COMMENT '创建人' ,
+    `update_user` VARCHAR(32) NOT NULL   COMMENT '更新人' ,
+    PRIMARY KEY (id)
+)  COMMENT = '线索池中的线索'
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_general_ci;
+
+CREATE INDEX idx_lead_id ON lead_pool_relation(lead_id ASC);
+CREATE INDEX idx_pool_id ON lead_pool_relation(pool_id ASC);
 
 -- set innodb lock wait timeout to default
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
