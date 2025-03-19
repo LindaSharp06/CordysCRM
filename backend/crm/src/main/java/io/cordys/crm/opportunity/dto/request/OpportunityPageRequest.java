903b6bff2ce813d4074f89f4aadb7b4cc6389577
WangXu10 <xu.wang@fit2cloud.com>
2025-03-19 10:16:57 +0800

    fix: searchType error

--- backend/crm/src/main/java/io/cordys/crm/opportunity/dto/request/OpportunityPageRequest.java
+++ backend/crm/src/main/java/io/cordys/crm/opportunity/dto/request/OpportunityPageRequest.java
@@ -1,7 +1,5 @@
 package io.cordys.crm.opportunity.dto.request;
 
-import io.cordys.common.constants.BusinessSearchType;
-import io.cordys.common.constants.EnumValue;
 import io.cordys.common.dto.BasePageRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -9,7 +7,6 @@ import lombok.Data;
 @Data
 public class OpportunityPageRequest extends BasePageRequest {
 
-    @EnumValue(enumClass = BusinessSearchType.class)
     @Schema(description = "搜索类型(ALL/SELF/DEPARTMENT/DEAL)")
     private String searchType ;
 }
