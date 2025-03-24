c04c210070e1d0969d200fe2f42f252459a03c83
WangXu10 <xu.wang@fit2cloud.com>
2025-03-24 17:57:46 +0800

    refactor: opportunity list return last stage

--- backend/crm/src/main/java/io/cordys/crm/opportunity/dto/response/OpportunityListResponse.java
+++ backend/crm/src/main/java/io/cordys/crm/opportunity/dto/response/OpportunityListResponse.java
@@ -43,6 +43,9 @@ public class OpportunityListResponse {
     @Schema(description = "联系人名称")
     private String contactName;
 
+    @Schema(description = "上次修改前的商机阶段")
+    private String lastStage;
+
     @Schema(description = "商机阶段")
     private String stage;
 
