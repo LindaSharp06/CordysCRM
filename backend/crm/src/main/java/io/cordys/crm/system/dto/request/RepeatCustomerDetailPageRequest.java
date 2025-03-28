d5c17c0d31cd8b72a445a48f7f7c7c3c86a10a23
guoyuqi <xiaomeinvG@126.com>
2025-03-28 17:45:27 +0800

    refactor: refactor repeat customer

---
+++ backend/crm/src/main/java/io/cordys/crm/system/dto/request/RepeatCustomerDetailPageRequest.java
@@ -0,0 +1,12 @@
+package io.cordys.crm.system.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class RepeatCustomerDetailPageRequest extends RepeatCustomerPageRequest {
+    
+    @Schema(description = "客户ID", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String id;
+
+}
