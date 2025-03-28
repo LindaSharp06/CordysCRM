af816e422cbc3b6432d910dfb5740f99168c0616
song-cc-rock <changchang.song@fit2cloud.com>
2025-03-28 10:38:28 +0800

    refactor: picture resolver

--- backend/crm/src/main/java/io/cordys/common/resolver/field/PictureResolver.java
+++ backend/crm/src/main/java/io/cordys/common/resolver/field/PictureResolver.java
@@ -1,11 +1,7 @@
 package io.cordys.common.resolver.field;
 
-import io.cordys.common.util.CommonBeanFactory;
 import io.cordys.common.util.JSON;
 import io.cordys.crm.system.dto.field.PictureField;
-import io.cordys.crm.system.service.PicService;
-
-import java.util.List;
 
 /**
  * @author song-cc-rock
@@ -19,11 +15,6 @@ public class PictureResolver extends AbstractModuleFieldResolver<PictureField>{
 
 	@Override
 	public Object parse2Value(PictureField selectField, String value) {
-		List<String> ids = JSON.parseArray(value, String.class);
-		PicService picService = CommonBeanFactory.getBean(PicService.class);
-		if (picService == null) {
-			return ids;
-		}
-		return picService.getPicInfos(ids);
+		return JSON.parseArray(value, String.class);
 	}
 }
