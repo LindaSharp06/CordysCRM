b8c8836769d485983c2bc29f1a8a0c7ec4b419b7
LindaSharp06 <linda0729sharp@outlook.com>
2025-04-11 16:00:08 +0800

    refactor: Allow cross-domain access

--- backend/crm/src/main/java/io/cordys/crm/system/notice/sse/SseController.java
+++ backend/crm/src/main/java/io/cordys/crm/system/notice/sse/SseController.java
@@ -3,10 +3,7 @@ package io.cordys.crm.system.notice.sse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
@@ -36,6 +33,7 @@ public class SseController {
      */
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @Operation(summary = "客户端订阅 SSE 事件流")
+    @CrossOrigin
     public SseEmitter subscribe(@RequestParam String userId, @RequestParam String clientId) {
         return sseService.addEmitter(userId, clientId);
     }
