334a7c2e8a0588252644baa7f31f19158d93aa59
WangXu10 <xu.wang@fit2cloud.com>
2025-04-21 11:01:07 +0800

    fix: fix zh-cn error
    --bug=1054988 --user=王旭 【系统】组织架构-批量选中用户-重置密码弹框提示信息错误 https://www.tapd.cn/34675357/s/1688267

--- frontend/packages/web/src/views/system/org/locale/zh-CN.ts
+++ frontend/packages/web/src/views/system/org/locale/zh-CN.ts
@@ -60,7 +60,7 @@ export default {
   'org.outsourcingUser': '外包',
   'org.checkIsOpenConfig': '请检查企业设置-同步组织架构功能配置!',
   'org.syncSuccess': '同步成功',
-  'org.batchResetPsdTip': '确认重置 {number} 名员工的密吗？',
+  'org.batchResetPsdTip': '确认重置 {number} 名员工的密码？',
   'org.batchToggleStatusTip': '确认 {enable} {number} 名员工吗？',
   'org.batchDeleteUserTip': '确认删除 {number} 名员工吗？',
   'org.checkSyncUserHoverTip': '当前组织架构为企业微信同步数据，请通过“同步组织架构”进行添加成员操作！',
