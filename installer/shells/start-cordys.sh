956140d2d43a455e4e5ba7eedc50903a7be3bc6d
LindaSharp06 <linda0729sharp@outlook.com>
2025-05-07 15:56:32 +0800

    build: Modify the installation configuration

--- installer/shells/start-cordys.sh
+++ installer/shells/start-cordys.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-chmod -R 777 /opt/cordys
-
 export JAVA_CLASSPATH=/app:/app/lib/*
 export JAVA_MAIN_CLASS=io.cordys.Application
 export CRM_VERSION=`cat /tmp/CRM_VERSION`
 
 sh /deployments/run-java.sh
-
-
