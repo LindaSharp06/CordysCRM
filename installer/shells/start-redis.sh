1deb2544e31c0f1f9f4b64b6eefe6d7b6550e60e
LindaSharp06 <linda0729sharp@outlook.com>
2025-05-08 17:00:05 +0800

    ci: Modify the installation script

--- installer/shells/start-redis.sh
+++ installer/shells/start-redis.sh
@@ -12,10 +12,12 @@ mkdir -p /opt/cordys/data/redis
 mkdir -p /opt/cordys/conf/redis
 
 # 检查 Redis 密码
-if [ -z "${REDIS_PASSWORD}" ]; then
+if [ -z "${CORDYS_REDIS_PASSWORD}" ]; then
     log "警告：CORDYS_REDIS_PASSWORD 环境变量未设置，使用默认密码 CordysCRM@redis"
-    REDIS_PASSWORD="CordysCRM@redis"
+    CORDYS_REDIS_PASSWORD="CordysCRM@redis"
 fi
+
+echo "requirepass ${CORDYS_REDIS_PASSWORD}" >> /opt/cordys/conf/redis/redis.conf
 
 # 启动 Redis 服务器
 log "启动 Redis 服务器..."
