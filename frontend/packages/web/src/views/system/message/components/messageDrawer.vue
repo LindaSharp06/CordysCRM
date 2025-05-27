16e2bb7772a5d0c81888ef6cd14929b0e0c6a401
xinxin.wu <xinxin.wu@fit2cloud.com>
2025-05-27 11:43:56 +0800

    fix: message refresh unread count

--- frontend/packages/web/src/views/system/message/components/messageDrawer.vue
+++ frontend/packages/web/src/views/system/message/components/messageDrawer.vue
@@ -312,6 +312,8 @@
     try {
       await setAllNotificationRead();
       messageListRef.value?.loadMessageList();
+      initMessageCount();
+      appStore.initMessage();
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(error);
