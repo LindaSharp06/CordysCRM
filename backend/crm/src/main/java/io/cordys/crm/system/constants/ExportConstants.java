be768e919f0a344c6d062e0c32860be26537ec13
WangXu10 <xu.wang@fit2cloud.com>
2025-06-20 14:13:12 +0800

    feat: add export task domain

---
+++ backend/crm/src/main/java/io/cordys/crm/system/constants/ExportConstants.java
@@ -0,0 +1,12 @@
+package io.cordys.crm.system.constants;
+
+public class ExportConstants {
+
+    public enum ExportType {
+        CUSTOMER, CLUE, OPPORTUNITY
+    }
+
+    public enum ExportStatus {
+        PREPARED, STOP, SUCCESS, ERROR
+    }
+}
