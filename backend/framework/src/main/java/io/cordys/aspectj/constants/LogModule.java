659eb3800fcb749af66ff30ef118b981bbbc67fa
WangXu10 <xu.wang@fit2cloud.com>
2025-07-04 15:13:52 +0800

    fix: clue combine search

--- backend/framework/src/main/java/io/cordys/aspectj/constants/LogModule.java
+++ backend/framework/src/main/java/io/cordys/aspectj/constants/LogModule.java
@@ -102,4 +102,14 @@ public class LogModule {
      * 客户公海
      */
     public static final String CUSTOMER_POOL = "CUSTOMER_POOL";
+
+    /**
+     * 仪表板模块
+     */
+    public static final String DASHBOARD_MODULE = "DASHBOARD_MODULE";
+
+    /**
+     * 仪表板
+     */
+    public static final String DASHBOARD = "DASHBOARD";
 }
