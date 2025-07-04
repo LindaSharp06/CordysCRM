16bf79270d5b8831fad1ac0508353e7af13f7f10
song-cc-rock <changchang.song@fit2cloud.com>
2025-07-04 11:18:16 +0800

    feat: Add dashboard module

--- backend/crm/src/main/resources/migration/1.0.2/dml/V1.0.2_2_1__data.sql
+++ backend/crm/src/main/resources/migration/1.0.2/dml/V1.0.2_2_1__data.sql
@@ -2,4 +2,8 @@
 
 SET SESSION innodb_lock_wait_timeout = 7200;
 
+-- 初始化仪表板菜单
+insert into sys_module value (UUID_SHORT(), '100001', 'dashboard', false, 7,
+                              'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000);
+
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
