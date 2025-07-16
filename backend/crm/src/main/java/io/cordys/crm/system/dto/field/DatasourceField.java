872c5572da133e0e3c961b0a7e2c2bfd0911cb27
song-cc-rock <changchang.song@fit2cloud.com>
2025-07-16 11:15:40 +0800

    feat: Datasource support filter

--- backend/crm/src/main/java/io/cordys/crm/system/dto/field/DatasourceField.java
+++ backend/crm/src/main/java/io/cordys/crm/system/dto/field/DatasourceField.java
@@ -2,6 +2,7 @@ package io.cordys.crm.system.dto.field;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.cordys.common.constants.EnumValue;
+import io.cordys.common.dto.condition.CombineSearch;
 import io.cordys.crm.system.constants.FieldSourceType;
 import io.cordys.crm.system.dto.field.base.BaseField;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,4 +16,6 @@ public class DatasourceField extends BaseField {
 	@EnumValue(enumClass = FieldSourceType.class)
 	@Schema(description = "数据源类型")
 	private String dataSourceType;
+	@Schema(description = "过滤条件")
+	private CombineSearch combineSearch;
 }
