afd681464123f8023e9a4115daf7108529b7c746
xinxin.wu <xinxin.wu@fit2cloud.com>
2025-07-18 18:48:52 +0800

    style: name style

--- frontend/packages/web/src/components/pure/crm-name-tooltip/index.vue
+++ frontend/packages/web/src/components/pure/crm-name-tooltip/index.vue
@@ -1,7 +1,11 @@
 <template>
   <NTooltip v-if="text" :delay="300" flip :disabled="!text">
     <template #trigger>
-      <div class="h-full overflow-hidden overflow-ellipsis whitespace-nowrap">{{ text }}</div>
+      <div class="overflow-hidden">
+        <div class="one-line-text h-full min-w-0 max-w-full items-center">
+          {{ text }}
+        </div>
+      </div>
     </template>
     {{ text }}
   </NTooltip>
