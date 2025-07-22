7e9d43edba09a165e2345f25a9daa93678490321
zhao <70558837+fit2-zhao@users.noreply.github.com>
2025-07-22 16:16:38 +0800

    chore: Default current system time

--- backend/crm/src/main/resources/migration/1.1.0/dml/V1.1.0_2_1__data.sql
+++ backend/crm/src/main/resources/migration/1.1.0/dml/V1.1.0_2_1__data.sql
@@ -8,6 +8,6 @@ insert into sys_module value (UUID_SHORT(), '100001', 'dashboard', false, 7,
 
 INSERT INTO `dashboard_module`(`id`, `organization_id`, `name`, `parent_id`, `pos`, `create_time`, `update_time`, `create_user`, `update_user`)
 VALUES
-    (UUID_SHORT(), '100001', '默认文件夹', 'NONE', 0, 1751600572000, 1751600572000, 'admin', 'admin');
+    (UUID_SHORT(), '100001', '默认文件夹', 'NONE', 0, UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, 'admin', 'admin');
 
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
