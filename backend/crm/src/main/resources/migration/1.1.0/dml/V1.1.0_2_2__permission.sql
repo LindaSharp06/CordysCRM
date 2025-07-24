e8a0f6ebe9958fa209420b1c7cbb9cdca47a51af
guoyuqi <xiaomeinvG@126.com>
2025-07-24 11:02:13 +0800

    refactor: permission sql

--- backend/crm/src/main/resources/migration/1.1.0/dml/V1.1.0_2_2__permission.sql
+++ backend/crm/src/main/resources/migration/1.1.0/dml/V1.1.0_2_2__permission.sql
@@ -6,11 +6,9 @@ INSERT INTO sys_role_permission
 VALUES (UUID_SHORT(), 'org_admin', 'DASHBOARD:READ'),
        (UUID_SHORT(), 'org_admin', 'DASHBOARD:ADD'),
        (UUID_SHORT(), 'org_admin', 'DASHBOARD:UPDATE'),
-       (UUID_SHORT(), 'org_admin', 'DASHBOARD:DELETE');
-
-INSERT INTO sys_role_permission
-(id, role_id, permission_id)
-VALUES (UUID_SHORT(), 'org_admin', 'LICENSE:READ'),
+       (UUID_SHORT(), 'org_admin', 'DASHBOARD:DELETE'),
+       (UUID_SHORT(), 'org_admin', 'LICENSE:READ'),
        (UUID_SHORT(), 'org_admin', 'LICENSE:EDIT');
+
 
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
