10b04f06d37ce5ada756efaa668b97ce54566274
song-cc-rock <changchang.song@fit2cloud.com>
2025-07-25 16:32:35 +0800

    fix: Dict miss type

--- backend/crm/src/main/resources/migration/1.1.1/dml/V1.1.1_2_1__data.sql
+++ backend/crm/src/main/resources/migration/1.1.1/dml/V1.1.1_2_1__data.sql
@@ -3,15 +3,15 @@
 SET SESSION innodb_lock_wait_timeout = 7200;
 
 -- init dict config
-INSERT INTO sys_dict values (UUID_SHORT(), '客户选择竞品', 'OPPORTUNITY_FAIL_RS', '100001',
+INSERT INTO sys_dict values (UUID_SHORT(), '客户选择竞品', 'OPPORTUNITY_FAIL_RS', 'TEXT', '100001',
 UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, 'admin', 'admin');
-INSERT INTO sys_dict values (UUID_SHORT(), '立项失败', 'OPPORTUNITY_FAIL_RS', '100001',
+INSERT INTO sys_dict values (UUID_SHORT(), '立项失败', 'OPPORTUNITY_FAIL_RS','TEXT','100001',
 UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, 'admin', 'admin');
-INSERT INTO sys_dict values (UUID_SHORT(), '决策链复杂', 'OPPORTUNITY_FAIL_RS', '100001',
+INSERT INTO sys_dict values (UUID_SHORT(), '决策链复杂', 'OPPORTUNITY_FAIL_RS','TEXT','100001',
 UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, 'admin', 'admin');
-INSERT INTO sys_dict values (UUID_SHORT(), '预算限制', 'OPPORTUNITY_FAIL_RS', '100001',
+INSERT INTO sys_dict values (UUID_SHORT(), '预算限制', 'OPPORTUNITY_FAIL_RS', 'TEXT','100001',
 UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, 'admin', 'admin');
-INSERT INTO sys_dict values (UUID_SHORT(), '需求变化', 'OPPORTUNITY_FAIL_RS', '100001',
+INSERT INTO sys_dict values (UUID_SHORT(), '需求变化', 'OPPORTUNITY_FAIL_RS', 'TEXT', '100001',
 UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, 'admin', 'admin');
 
 -- opportunity fail reason => sys_dict.fail.id
