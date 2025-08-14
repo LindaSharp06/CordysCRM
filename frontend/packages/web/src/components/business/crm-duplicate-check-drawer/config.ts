5dd70fbece0f0045eb76d8e75e472380f7fb2fa0
xinxin.wu <xinxin.wu@fit2cloud.com>
2025-08-14 18:13:20 +0800

    refactor: global search permission

--- frontend/packages/web/src/components/business/crm-duplicate-check-drawer/config.ts
+++ frontend/packages/web/src/components/business/crm-duplicate-check-drawer/config.ts
@@ -3,6 +3,7 @@ import { ModuleConfigEnum } from '@lib/shared/enums/moduleEnum';
 import { useI18n } from '@lib/shared/hooks/useI18n';
 
 import { useAppStore } from '@/store';
+import { hasAnyPermission } from '@/utils/permission';
 
 const { t } = useI18n();
 const appStore = useAppStore();
@@ -12,36 +13,44 @@ const scopedOptions = [
     label: t('crmFormDesign.customer'),
     value: FormDesignKeyEnum.SEARCH_GLOBAL_CUSTOMER,
     moduleKey: ModuleConfigEnum.CUSTOMER_MANAGEMENT,
+    permission: ['CUSTOMER_MANAGEMENT:READ'],
   },
   {
     label: t('crmFormDesign.contract'),
     value: FormDesignKeyEnum.SEARCH_GLOBAL_CONTACT,
     moduleKey: ModuleConfigEnum.CUSTOMER_MANAGEMENT,
+    permission: ['CUSTOMER_MANAGEMENT_CONTACT:READ'],
   },
   {
     label: t('module.openSea'),
     value: FormDesignKeyEnum.SEARCH_GLOBAL_PUBLIC,
     moduleKey: ModuleConfigEnum.CUSTOMER_MANAGEMENT,
+    permission: ['CUSTOMER_MANAGEMENT_POOL:READ'],
   },
   {
     label: t('crmFormDesign.clue'),
     value: FormDesignKeyEnum.SEARCH_GLOBAL_CLUE,
     moduleKey: ModuleConfigEnum.CLUE_MANAGEMENT,
+    permission: ['CLUE_MANAGEMENT:READ'],
   },
   {
     label: t('module.cluePool'),
     value: FormDesignKeyEnum.SEARCH_GLOBAL_CLUE_POOL,
     moduleKey: ModuleConfigEnum.CLUE_MANAGEMENT,
+    permission: ['CLUE_MANAGEMENT_POOL:READ'],
   },
   {
     label: t('module.businessManagement'),
     value: FormDesignKeyEnum.SEARCH_GLOBAL_OPPORTUNITY,
     moduleKey: ModuleConfigEnum.BUSINESS_MANAGEMENT,
+    permission: ['OPPORTUNITY_MANAGEMENT:READ'],
   },
 ];
 
 export const lastScopedOptions = computed(() =>
-  scopedOptions.filter((e) => appStore.moduleConfigList.find((m) => m.moduleKey === e.moduleKey && m.enable))
+  scopedOptions.filter((e) =>
+    appStore.moduleConfigList.find((m) => m.moduleKey === e.moduleKey && m.enable && hasAnyPermission(e.permission))
+  )
 );
 
 export default {};
