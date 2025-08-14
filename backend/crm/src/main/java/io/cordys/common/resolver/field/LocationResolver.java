712c9bcc5589842d68ce1d2548f8bbbe8657376c
WangXu10 <xu.wang@fit2cloud.com>
2025-08-14 11:31:29 +0800

    fix: export location
    --bug=1060396 --user=王旭 【线索】地址字段为「国家-省-市」/「国家-省-市-区」-导出线索时-地址字段结尾也显示了- https://www.tapd.cn/34675357/s/1754380

--- backend/crm/src/main/java/io/cordys/common/resolver/field/LocationResolver.java
+++ backend/crm/src/main/java/io/cordys/common/resolver/field/LocationResolver.java
@@ -60,7 +60,7 @@ public class LocationResolver extends AbstractModuleFieldResolver<LocationField>
         //编码
         String code = value.substring(0, value.indexOf(SPILT_STR));
         //描述
-        String detail = value.substring(value.indexOf(SPILT_STR));
+        String detail = value.substring(value.indexOf(SPILT_STR) + 1);
 
         String regionName = StringUtils.EMPTY;
         List<RegionCode> regionCode = getRegionCodes();
@@ -101,6 +101,9 @@ public class LocationResolver extends AbstractModuleFieldResolver<LocationField>
         }
 
 
+        if (StringUtils.isBlank(detail)) {
+            return regionName;
+        }
         return regionName + detail;
     }
 
