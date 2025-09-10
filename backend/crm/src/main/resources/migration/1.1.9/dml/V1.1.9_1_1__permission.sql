51f4c58627d8be85205f02fc460c80b900712806
WangXu10 <xu.wang@fit2cloud.com>
2025-09-10 16:48:29 +0800

    fix: init permissions
    --bug=1061640 --user=王旭 【个人中心】当前用户为管理员-未展示API KEY入口 https://www.tapd.cn/34675357/s/1770903

--- backend/crm/src/main/resources/migration/1.1.9/dml/V1.1.9_1_1__permission.sql
+++ backend/crm/src/main/resources/migration/1.1.9/dml/V1.1.9_1_1__permission.sql
@@ -3,4 +3,16 @@ SET SESSION innodb_lock_wait_timeout = 7200;
 
 -- init import permissions for admin roles
 
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'PERSONAL_API_KEY:READ');
+
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'PERSONAL_API_KEY:ADD');
+
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'PERSONAL_API_KEY:UPDATE');
+
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'PERSONAL_API_KEY:DELETE');
+
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
