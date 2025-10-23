35b37767dfac8421d3df018e21bacd1633897aab
WangXu10 <xu.wang@fit2cloud.com>
2025-10-23 17:42:38 +0800

    refactor: DE config detail

--- backend/crm/src/main/java/cn/cordys/crm/integration/dataease/dto/DeConfigDetailDTO.java
+++ backend/crm/src/main/java/cn/cordys/crm/integration/dataease/dto/DeConfigDetailDTO.java
@@ -10,16 +10,10 @@ public class DeConfigDetailDTO {
     private String agentId;
     @Schema(description = "应用密钥", requiredMode = Schema.RequiredMode.REQUIRED)
     private String appSecret;
-    @Schema(description = "DE账号")
-    private String deAccount;
     @Schema(description = "回调地址")
     private String redirectUrl;
     @Schema(description = "是否验证通过")
     private Boolean verify;
-    @Schema(description = "DE模块嵌入")
-    private Boolean deModuleEmbedding;
-    @Schema(description = "DE外链集成")
-    private Boolean deLinkIntegration;
     @Schema(description = "DE自动同步")
     private Boolean deAutoSync;
     @Schema(description = "DEAccessKey")
