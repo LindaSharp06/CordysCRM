533ff0d1e28c7cee7ed2bc050a30bde6ef2d6d00
WangXu10 <xu.wang@fit2cloud.com>
2025-10-27 15:45:11 +0800

    refactor: alter table field length

--- backend/crm/src/main/resources/migration/1.3.0/ddl/V1.3.0_2__ga_ddl.sql
+++ backend/crm/src/main/resources/migration/1.3.0/ddl/V1.3.0_2__ga_ddl.sql
@@ -1,7 +1,7 @@
 -- set innodb lock wait timeout
 SET SESSION innodb_lock_wait_timeout = 7200;
 
-ALTER TABLE dashboard CHANGE COLUMN resource_id resource_url VARCHAR(100) NOT NULL COMMENT '仪表板url';
+ALTER TABLE dashboard CHANGE COLUMN resource_id resource_url VARCHAR(500) NOT NULL COMMENT '仪表板url';
 
 
 -- set innodb lock wait timeout to default
